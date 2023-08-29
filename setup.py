20df1efa862093e3004f594121bd6d5559d00088
ddavidgleason <143480184+ddavidgleason@users.noreply.github.com>
2023-08-29 23:11:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='myexecutable',
     version='0.2',
-    packages=find_packages(where='src'),
-    install_requires=[],
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    python_requires='>=3',
     author='David',
     author_email='ddickson@gleason.com',
     description='A simple Python package',
