6aede4cae7e233d7a16047594d8cdeeedcf0edf5
ddavidgleason <143480184+ddavidgleason@users.noreply.github.com>
2023-08-29 23:10:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pymodule33',
+    name='myexecutable',
     version='0.2',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
     install_requires=[],
     author='David',
     author_email='ddickson@gleason.com',
